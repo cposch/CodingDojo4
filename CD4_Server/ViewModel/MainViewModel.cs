f65e93af694909cf1aaa22f5fa83e0435166464f
cposch <cgposch@gmail.com>
2017-12-04 02:35:19 +0100

    Add functionality but server don't start

--- CD4_Server/ViewModel/MainViewModel.cs
+++ CD4_Server/ViewModel/MainViewModel.cs
@@ -1,12 +1,84 @@
+using CD4_Server.Connection;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
 
 namespace CD4_Server.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private Server server;
+        private const int port = 8090;
+        private const string ip = "127.0.0.1";
+        private bool isConnected = false;
+
+        public RelayCommand StartBtnClickCmd { get; set; }
+        public RelayCommand StopBtnClickCmd { get; set; }
+        public RelayCommand DropClientBtnClickCmd { get; set; }
+        public RelayCommand SaveToLogBtnClickCmd { get; set; }
+        public ObservableCollection<string> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+
+        public string SelectedUser { get; set; }
+
+        public int NoOfReceivedMessages
+        {
+            get
+            {
+                return Messages.Count;
+            }
+        }
+
         public MainViewModel()
         {
-            
+            Messages = new ObservableCollection<string>();
+            Users = new ObservableCollection<string>();
+
+            //set command for start button
+            StartBtnClickCmd = new RelayCommand(
+                () =>
+                {
+                    server = new Server(ip, port, UpdateGuiWithNewMessage);
+                    server.StartAccepting();
+                    isConnected = true;
+                },
+                () => { return !isConnected; });
+
+            //set command for stop button
+            StopBtnClickCmd = new RelayCommand(
+                //action for execute
+                () =>
+                {
+                    server.StopAccepting();
+                    isConnected = false;
+                },
+                //can execute
+                () => { return isConnected; });
+
+            //init Command for Drop button with CanExecute statement
+            DropClientBtnClickCmd = new RelayCommand(() =>
+            {
+                server.DisconnectSpecificClient(SelectedUser);
+                Users.Remove(SelectedUser); // remove from GUI listbox
+            },
+                () => { return (SelectedUser != null); });
+        }
+
+        public void UpdateGuiWithNewMessage(string message)
+        {
+            //switch thread to GUI thread to write to GUI
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                string name = message.Split(':')[0];
+                if (!Users.Contains(name))
+                {//not in list => add it
+                    Users.Add(name);
+                }
+                //write message
+                Messages.Add(message);
+                //do this to inform the GUI about the update of the received message counter!
+                RaisePropertyChanged("NoOfReceivedMessages");
+            });
         }
     }
 }
--- CodingDojo4/ViewModel/MainViewModel.cs
+++ CodingDojo4/ViewModel/MainViewModel.cs
@@ -1,23 +1,63 @@
+using CodingDojo4.Connection;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace CodingDojo4.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
         private bool isConnected = false;
-
+        private Client clientcom;
         public string ChatName { get; set; }
         public string Message { get; set; }
-        public ObservableCollection<string> RecievedMessages { get; set; }
-        public RelayCommand ConnectBtnClickedCmd { get; set; }
+        public ObservableCollection<string> ReceivedMessages { get; set; }
+        public RelayCommand ConnectBtnClickCmd { get; set; }
         public RelayCommand SendBtnClickCmd { get; set; }
 
 
         public MainViewModel()
         {
+            Message = "";
+            ReceivedMessages = new ObservableCollection<string>();
+            //init Command for Connect button with canExecute statement
+            ConnectBtnClickCmd = new RelayCommand(
+                () =>
+                {
+                    isConnected = true;
+                    clientcom = new Client("127.0.0.1", 8090, new Action<string>(NewMessageReceived), ClientDissconnected);
 
+                },
+                () =>
+                {
+                    return (!isConnected);
+                });
+            //init Command for Send button with CanExecute statement
+            SendBtnClickCmd = new RelayCommand(
+                () => {
+                    clientcom.Send(ChatName + ": " + Message);
+                    //write own message to GUI
+                    ReceivedMessages.Add("YOU: " + Message);
+                }, () => { return (isConnected && Message.Length >= 1); });
+        }
+
+        private void ClientDissconnected()
+        {
+            isConnected = false;
+            //do this to force the update of the button visibility otherwise change is done after focus change (clicking into gui)
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        private void NewMessageReceived(string message)
+        {
+            //write new message in Collection to display in GUI
+            //switch thread to GUI thread to avoid problems
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                ReceivedMessages.Add(message);
+            });
         }
     }
 }
